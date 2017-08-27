8ffd70b5c7af4ae242d71a02acbd2ec650837f3d
polyren <rendu2008@gmail.com>
2017-08-27 04:04:55 -0400

    MENU NAVIGATION

--- js/fullpagescroll.js
+++ js/fullpagescroll.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    loadData();      
+    $(window).on('hashchange',function(){
+        render(decodeURI(window.location.hash));
+    })
+    loadData(); 
+//    $(window).on('hashchange',function(){
+//        render(decodeURI(window.location.hash));
+//    })
 });
 
     var homeprojects=[]; 
@@ -32,7 +38,8 @@ $(document).ready(function() {
         })
         .fail(function(){
             console.log("commfailed");
-        });    
+        });   
+        $(window).trigger('hashchange');
     }
     
     function generatehomeProjectsHTML(data){
@@ -46,6 +53,12 @@ $(document).ready(function() {
         homepagelist.append(template(data));
         homepagedotlist.append(template2(data));
         
+        homepagelist.find('.check-out').on('click',function(e){
+            e.preventDefault();
+            var resentIndex=$(this).data('index');
+            window.location.hash='resent/'+resentIndex;
+        })
+        
         var homeprojectcont= homeprojects.length; 
         for( i=homeprojectcont-homeprojectcont;i <= (homeprojectcont);i++){
             homepagedotsanchors[i]="page"+(i+1)+"dot";
@@ -56,28 +69,144 @@ $(document).ready(function() {
         $('#scene').fullpage({
             anchors:homepagedotsanchors,
             menu:'#homepageMenu',
+            lockAnchors: true,
         });   
-
+ 
         var checkoutbar = $('.check-out');
         var checkoutback = $('.check-out-back');      
         checkoutbar.hover(function(){
-        checkoutback.toggleClass('active');
-        })
+        checkoutback.toggleClass('active');  
+       })
     }
 
     function generateprogramProjectsHTML(data){
         var programnamelist=$('#program-list');
         var programnames=$('#programs').html();
         var pronametemplate=Handlebars.compile(programnames);
-        programnamelist.append(pronametemplate(data));            
+        programnamelist.append(pronametemplate(data));
+        
+        programnamelist.find('li').on('click',function(e){
+            e.preventDefault();
+            var programsIndex=$(this).data('index');
+            window.location.hash='programs/'+programsIndex;
+            console.log(programsIndex);
+        })
     }
 
     function generatecommProjectsHTML(data){
         var commnamelist=$('#comm-list');
         var commnames=$('#comms').html();
         var commtemplate=Handlebars.compile(commnames);
-        commnamelist.append(commtemplate(data));            
+        commnamelist.append(commtemplate(data)); 
+        commnamelist.find('li').on('click',function(e){
+            e.preventDefault();
+            var programsIndex=$(this).data('index');
+            window.location.hash='communication/'+programsIndex;
+            console.log(programsIndex);
+        })
     }
-    
-        
+
+    function render(url){
+        var temp=url.split('/')[0];
+        $('body .page ').removeClass('visible');
+        $('#resume-big').removeClass('hilight');
+        $('#homepage-big').removeClass('hilight');
+        $('#projects-big').removeClass('hilight');
+        $('#program-title').removeClass('hilight');
+        $('#projects-big').removeClass('hilight');
+        $('#comm-title').removeClass('hilight');
+        var map={
+            '':function(){
+                renderhomepage();
+            },
+            
+            '#PG':function(){
+                renderpgpage();
+            },
+            '#CD':function(){
+                rendercdpage();
+            },
+            
+            '#Resume':function(){
+                renderresumepage();
+
+            },
+            '#programs':function(){
+                var index=url.split('#programs/')[1].trim();
+                renderSingleProgramPage(index,programprojects);
+            },
+            '#communication':function(){
+                var index=url.split('#communication/')[1].trim();
+                renderSingleCommunicationPage(index,commprojects);
+            },
+            '#resent':function(){
+                var index=url.split('#resent/')[1].trim();
+                renderresentPage(index,homeprojects);
+            }
+        };
+        if(map[temp]){
+            map[temp]();
+        }
+        else{
+            console.log("cannotrenderanypage");
+        }
+    }
+
+function renderhomepage(){
+    $(".homepage").addClass('visible');
+    $("#homepageMenu").addClass('visible');
+    menuon();
+    $("#homepage-big").addClass('hilight');
+    $('#scene').fullpage.setAllowScrolling(true);
+}
+function renderresumepage(){
+    $(".resumepage").addClass('visible');
+    menuvisual=true;
+    menuoff();
+    $("#resume-big").addClass('hilight');
+    $('#scene').fullpage.setAllowScrolling(false);
+}
+
+function renderpgpage(){
+    $(".PGpage").addClass('visible');
+    menuvisual=true;
+    menuoff();
+    $("#projects-big").addClass('hilight');
+    $("#program-title").addClass('hilight');
+    $('#scene').fullpage.setAllowScrolling(false);
+ 
+}
+
+function rendercdpage(){
+    $(".CDpage").addClass('visible');
+    menuvisual=true;
+    menuoff();
+    $("#projects-big").addClass('hilight');
+    $("#comm-title").addClass('hilight');
+    $('#scene').fullpage.setAllowScrolling(false);
+}
+
+
+function renderSingleProgramPage(index,data){
+     $(".singlepage").addClass('visible');
+    menuvisual=true;
+    menuoff();
+    $('#scene').fullpage.setAllowScrolling(false);
+
+
+}
+function renderSingleCommunicationPage(index,data){
+     $(".singlepage").addClass('visible');
+    menuvisual=true;
+    menuoff();
+    $('#scene').fullpage.setAllowScrolling(false);
+
+}
+
+function renderresentPage(index,data){
+     $(".singlepage").addClass('visible');
+    homeoff();
+    console.log("renderingresentpage");
+    $('#scene').fullpage.setAllowScrolling(false);
+}
 
